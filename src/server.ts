ba12e5e16a21223c55192c70e661e8ab8a2fe403
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-11-25 02:23:13 +0600

    Eslint prettier conflict fix

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import config from "./config/development";
 import mongoose from "mongoose";
 import app from "./app";
 
-main().catch((err) => console.log(err));
+main().catch((err) => {
+  console.log(err);
+});
 
-async function main() {
+async function main(): Promise<void> {
   await mongoose.connect(config.DATABASE_URL as string);
 
   app.listen(config.PORT, () => {
